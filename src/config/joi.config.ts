8d8961bb506b2422c8e264541ba11f040764ccfd
mahiru <dr.glasdou@gmail.com>
2023-08-24 17:35:35 -0600

    update: env.template and env validation

--- src/config/joi.config.ts
+++ src/config/joi.config.ts
@@ -7,6 +7,10 @@ export const JoiValidationSchema = Joi.object({
   DB_HOST: Joi.string().required(),
   DB_USERNAME: Joi.string().required(),
   PORT: Joi.number().default(3000),
-  JWT_SECRET: Joi.string().required(),
   STATE: Joi.string().default('dev'),
+  JWT_SECRET: Joi.string().required(),
+  JWT_EXPIRATION_TIME: Joi.string().required(),
+  CLOUDINARY_CLOUD_NAME: Joi.string().required(),
+  CLOUDINARY_API_KEY: Joi.string().required(),
+  CLOUDINARY_API_SECRET: Joi.string().required(),
 });
